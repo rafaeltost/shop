d3662f34154d9e92e1512835e3c09699475f671c
Rafael <rafaeltost@gmail.com>
2022-08-22 01:14:30 -0300

    added snackbar

--- lib/components/product_grid_item.dart
+++ lib/components/product_grid_item.dart
@@ -37,6 +37,7 @@ class ProductGridItem extends StatelessWidget {
           trailing: IconButton(
             onPressed: () {
               cart.addItem(product);
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(
                   content: const Text('Produto adicionado com sucesso.'),
